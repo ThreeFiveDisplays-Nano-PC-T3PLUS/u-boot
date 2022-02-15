4d600d065ccc9179d346966b1be8d63868760909
ThreeFiveDisplays-Nano-PC-T3PLUS <88690798+ThreeFiveDisplays-Nano-PC-T3PLUS@users.noreply.github.com>
2022-02-15 14:02:35 -0700

    Update board.c

--- board/s5p6818/nanopi3/board.c
+++ board/s5p6818/nanopi3/board.c
@@ -177,16 +177,16 @@ static void bd_onewire_init(void)
 
 static void bd_lcd_init(void)
 {
-	struct nxp_lcd *cfg;
+/*	struct nxp_lcd *cfg;
 	int id;
 	int ret;
 
-	id = onewire_get_lcd_id();
+	id = onewire_get_lcd_id();*/
 	/* -1: onwire probe failed
 	 *  0: bad
 	 * >0: identified */
 
-	ret = bd_setup_lcd_by_id(id);
+/*	ret = bd_setup_lcd_by_id(id);
 	if (id <= 0 || ret != id) {
 		printf("Panel: N/A (%d)\n", id);
 		bd_setup_lcd_by_name("HDMI720P60");
@@ -198,7 +198,7 @@ static void bd_lcd_init(void)
 
 		if (cfg->gpio_init)
 			cfg->gpio_init();
-	 } 
+	 } */
 }
 
 static int mac_read_from_generic_eeprom(u8 *addr)
