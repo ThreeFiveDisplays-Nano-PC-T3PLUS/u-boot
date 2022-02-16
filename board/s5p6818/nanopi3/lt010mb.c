e433853053135cdf29def05f70696e164574576a
ThreeFiveDisplays-Nano-PC-T3PLUS <88690798+ThreeFiveDisplays-Nano-PC-T3PLUS@users.noreply.github.com>
2022-02-16 15:56:34 -0700

    Update lt010mb.c

--- board/s5p6818/nanopi3/lt010mb.c
+++ board/s5p6818/nanopi3/lt010mb.c
@@ -83,6 +83,11 @@ static void lt101mb_set_sequence(struct lt101mb *ctx)
 		return;
 
 	mdelay(18);
+	lt101mb_dcs_write_seq_static(ctx, 0x11);
+	mdelay(120);
+	lt101mb_dcs_write_seq_static(ctx, 0x29);
+	mdelay(50);
+	
  /*       printf("Start of DSC Mipi Commands to Panel\n");
 	lt101mb_dcs_write_seq_static(ctx, 0xFF, 0x98, 0x81, 0x03); 
 	 printf("Finished sending first DSC Command to Panel\n");
